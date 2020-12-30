bbce2b46d824fcb1d8b0c047217f6592a009efc9
gnommmer21 <cgeetacharan@gmail.com>
2020-12-30 10:16:03 +0000

    fix for ingress

--- server.js
+++ server.js
@@ -9,30 +9,30 @@ function init() {
 
   app.set('port', (process.env.PORT || 5000))
 
-  app.get('/', (request, response) => {
+  app.get('/ais-api', (request, response) => {
     response.sendFile(path.join(__dirname + '/index.html'));
   });
 
-  app.get('/getLastPositionFromVF/:mmsi', (req, res) => {
+  app.get('/ais-api/getLastPositionFromVF/:mmsi', (req, res) => {
     api.getLocationFromVF(req.params.mmsi, (result) => {
       res.send(result);
     });
   });
 
-  app.get('/getLastPositionFromMT/:mmsi', (req, res) => {
+  app.get('/ais-api/getLastPositionFromMT/:mmsi', (req, res) => {
     api.getLocationFromMT(req.params.mmsi, (result) => {
       res.send(result);
     });
   });
 
-  app.get('/getLastPosition/:mmsi', (req, res) => {
+  app.get('/ais-api/getLastPosition/:mmsi', (req, res) => {
     api.getLocation(req.params.mmsi, (result) => {
       res.send(result);
     });
   });
 
   // e.g. /getVesselsInArea/WMED,EMED
-  app.get('/getVesselsInArea/:area', async (req, res) => {
+  app.get('/ais-api/getVesselsInArea/:area', async (req, res) => {
     const result = await areaApi.fetchVesselsInArea(req.params.area.split(','), (result) => {
       res.json(result);
     });
